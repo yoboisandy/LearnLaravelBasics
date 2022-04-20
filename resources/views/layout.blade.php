95d8a5f3fd72fac8d6986d132c0fdd626b8524b3
Sandeep Sharma <1949sandeepsharma52@gmail.com>
2022-04-20 22:19:03 +0545

    Layout file added

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Home</title>
+</head>
+<body>
+    <nav>
+        <ul style="display: flex;list-style: none;gap: 40px">
+            <li><a href="/home">Home</a></li>
+            <li><a href="/about/sandeep">About</a></li>
+            <li><a href="/directives">Directives</a></li>
+        </ul>
+    </nav>
+    <div style="margin: 40px">
+        @yield('content')
+    </div>
+    <footer style="padding: 5px 40px; background-color: gray;">
+        <h3>
+            This is a footer
+        </h3>
+    </footer>
+</body>
+</html>
