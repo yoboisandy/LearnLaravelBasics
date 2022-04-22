c121649c09df81e61ab658939c56810138a18c25
Sandeep Sharma <1949sandeepsharma52@gmail.com>
2022-04-22 21:48:01 +0545

    created a customer model

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\DemoController;
 use App\Http\Controllers\RegistrationController;
+use App\Models\Customer;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -31,3 +32,9 @@ Route::get('/', [DemoController::class, 'index']);
 Route::get('/about/{name}', '\App\Http\Controllers\DemoController@about');
 Route::get('/register', [RegistrationController::class, 'registrationForm']);
 Route::post('/register', [RegistrationController::class, 'submitForm']);
+
+Route::get('/customers', function () {
+    $customers = Customer::all();
+    echo "<pre>";
+    print_r($customers->toArray());
+});
