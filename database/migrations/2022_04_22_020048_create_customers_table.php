99ae3b80246135ed226e1d688497e08249ed3c8b
Sandeep Sharma <1949sandeepsharma52@gmail.com>
2022-04-22 08:04:27 +0545

    created a customers table and added columns

---
+++ database/migrations/2022_04_22_020048_create_customers_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email');
+            $table->enum('gender', ["M", "F", "O"]);
+            $table->text('address');
+            $table->date('dob')->nullable();
+            $table->string('password');
+            $table->boolean('status')->default(1);
+            $table->integer('points')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customers');
+    }
+};
