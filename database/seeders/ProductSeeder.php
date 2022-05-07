c8ad6545983fce1b9f4d25af17da9f24c1bfdcea
Sandeep Sharma <1949sandeepsharma52@gmail.com>
2022-05-07 23:34:57 +0545

    added product seeder and used faker in it

---
+++ database/seeders/ProductSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Product;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
+
+class ProductSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Faker::create();
+        $product = new Product();
+        $product->name = $faker->name;
+        $product->description = $faker->text(100);;
+        $product->price = $faker->randomNumber(3, true);
+        $product->save();
+    }
+}
